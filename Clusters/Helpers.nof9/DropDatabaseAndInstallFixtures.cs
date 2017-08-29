6d48fe71362fcf620d6d30cf597a0d09e031ffa0
Tom Fox <tom@1234most.co.uk>
2017-08-29 13:47:26 +0100

    + old files (revised) into Helpers.nof9

---
+++ Clusters/Helpers.nof9/DropDatabaseAndInstallFixtures.cs
@@ -0,0 +1,19 @@
+﻿using System.Data.Entity;
+
+namespace Helpers.nof9
+{
+    public class DropDatabaseAndInstallFixtures<T> : DropCreateDatabaseAlways<T> where T : DbContext
+    {
+        public DropDatabaseAndInstallFixtures()
+        {
+            IsInitialized = true;
+        }
+
+        public static bool IsInitialized { get; set; }
+
+        protected override void Seed(T context)
+        {
+            IsInitialized = false;
+        }
+    }
+}
---
+++ Clusters/Helpers.nof9/TestDefaultAuthorizer.cs
@@ -0,0 +1,27 @@
+﻿using System.Security.Principal;
+using NakedObjects.Security;
+
+namespace Helpers.nof9
+{
+    /// <summary>
+    /// For use in testing authorization within XATs; authorizes ALL requests
+    /// </summary>
+    public class TestDefaultAuthorizer : ITypeAuthorizer<object>
+    {
+
+        public void Init() { }
+
+
+        public bool IsEditable(IPrincipal principal, object target, string memberName)
+        {
+            return true;
+        }
+
+        public bool IsVisible(IPrincipal principal, object target, string memberName)
+        {
+            return true;
+        }
+
+        public void Shutdown() { }
+    }
+}
