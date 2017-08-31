e7a5681c8d9393d04a2a9697affa3f526de5d45c
Richard Pawson <rpawson@nakedobjects.net>
2017-08-31 20:20:42 +0100

    Changed one test -  for possible simpler solution to datetime issue

--- Clusters/Cluster.Tasks.Test/TasksXAT.cs
+++ Clusters/Cluster.Tasks.Test/TasksXAT.cs
@@ -441,11 +441,14 @@ namespace Cluster.Tasks.Test
             status.AssertTitleIsEqual("Suspended");
             unsuspend.AssertIsVisible();
 
-			//until.AssertIsVisible().AssertIsNotEmpty().AssertValueIsEqual("08/01/2000 00:00:00");
-			var expectedText = $"{UtcAndToStringSortable(new DateTime(2000, 1, 8, 0, 0, 0, DateTimeKind.Utc))}";
-			until.AssertIsVisible().AssertIsNotEmpty().AssertValueIsEqual(expectedText);
-
-			var notes = task.GetPropertyByName("Notes").Title;
+            //Original
+            //until.AssertIsVisible().AssertIsNotEmpty().AssertValueIsEqual("08/01/2000 00:00:00");
+            //Tom's change
+            //var expectedText = $"{UtcAndToStringSortable(new DateTime(2000, 1, 8, 0, 0, 0, DateTimeKind.Utc))}";
+            //until.AssertIsVisible().AssertIsNotEmpty().AssertValueIsEqual(expectedText);
+            //Richard's proposal
+            until.AssertIsVisible().AssertIsNotEmpty().AssertValueIsEqual((new DateTime(2000, 1, 8).ToString()));
+            var notes = task.GetPropertyByName("Notes").Title;
             StringAssert.EndsWith(notes, "Suspended: foo");
             AssertLastHistoryEntryHasValues(task, "Suspended", "Charlie", "Test");
         }
